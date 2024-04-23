fed391313eb70621108ed4cd450252211b5f57c7
Rajat Rahangdale <rjtrahangdale@gmail.com>
2024-04-23 19:22:52 +0530

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
+// import Home from "pages/Home";
 import NotFound from "pages/NotFound";
 import Desktop from "pages/Desktop";
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
+    { path: "/", element: <Desktop /> },
     { path: "*", element: <NotFound /> },
     {
       path: "desktop",
